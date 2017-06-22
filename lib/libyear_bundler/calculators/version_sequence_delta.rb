acae87dc9c024dc28288e3e995f101efba8ba9e9
Shane Cavanaugh <shane@patientdev.co>
2017-06-22 13:16:43 -0400

    Remove inadvertent line

--- lib/libyear_bundler/calculators/version_sequence_delta.rb
+++ lib/libyear_bundler/calculators/version_sequence_delta.rb
@@ -9,7 +9,6 @@ module Calculators
         uri = URI.parse("https://rubygems.org/api/v1/versions/#{gem[:name]}.json")
         response = Net::HTTP.get_response(uri)
         versions = JSON.parse(response.body).map { |version| version["number"] }
-        require 'pry'
         newest_seq = versions.index(gem[:newest][:version])
         installed_seq = versions.index(gem[:installed][:version])
         installed_seq - newest_seq

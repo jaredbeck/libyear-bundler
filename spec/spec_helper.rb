cbcd05f7f4ac9662f5205a9196ae350001773956
Jared Beck <jared@jaredbeck.com>
2022-05-02 01:14:58 -0400

    Ruby.all_versions: HTTP timeout, response code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ SimpleCov.start
 
 require 'libyear_bundler'
 require 'rspec'
+
+require 'webmock/rspec'
+WebMock.disable_net_connect!
+
+require 'vcr'
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end

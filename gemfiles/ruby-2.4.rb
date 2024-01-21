dd3cd9d17cecbad8a5f25f7deb485e302207ae3e
Jared Beck <jared@jaredbeck.com>
2024-01-21 13:37:18 -0500

    Test: Ruby 2.4 can use simplecov 1.18

--- gemfiles/ruby-2.4.rb
+++ gemfiles/ruby-2.4.rb
@@ -2,6 +2,6 @@ source 'https://rubygems.org'
 
 gem "bundler", ">= 1.14", "< 3"
 gem "rspec", "~> 3.9"
-gem "simplecov", "~> 0.17.1"
+gem "simplecov", "~> 0.18.5"
 gem "webmock", "~> 3.14.0"
 gem "vcr", "~> 5.1.0"

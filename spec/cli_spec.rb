3dd12d761ff9289faca5978143ad2af83690cd4f
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-03-16 20:52:15 -0400

    Add one more scenario and change value of expectations

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -5,7 +5,7 @@ module LibyearBundler
     describe "#run" do
       it 'outputs the libyear grand-total' do
         VCR.use_cassette("03-Gemfile-Rails") do
-          message = "5.2\n"
+          message = "11.4\n"
 
           expect do
             described_class.new(["spec/fixtures/03/Gemfile", "--grand-total"]).run
@@ -14,17 +14,31 @@ module LibyearBundler
       end
 
       context "--ignore list is passed" do
+        before do
+          @ignored_gems = %w[actioncable actionmailbox actionmailer actionpack actiontext actionview activejob activemodel activerecord activestorage activesupport railties]
+        end
+
         it "outputs a smaller libyear grand-total number because of the ignore" do
           VCR.use_cassette("03-Gemfile-Rails") do
-            message = "0.4\n"
+            message = "5.4\n"
 
             expect do
               described_class.new(["spec/fixtures/03/Gemfile", "--grand-total", "--ignore",
-                                   "actioncable,actionmailbox,actionmailer,actionpack,"\
-                                   "actiontext,actionview,activejob,activemodel,"\
-                                   "activerecord,activestorage,activesupport,"\
-                                   "railties"]).run
+                                   @ignored_gems.join(",")]).run
             end.to output(message).to_stdout
+          end
+        end
+
+        it "doesn't include the filtered gems in the report" do
+          VCR.use_cassette("03-Gemfile-Rails") do
+            allow($stdout).to receive(:write)
+
+            report = described_class.new(
+              ["spec/fixtures/03/Gemfile", "--ignore",
+              @ignored_gems.join(",")]
+            ).run
+
+            expect(report.to_h[:gems].map(&:name)).to_not include(*@ignored_gems)
           end
         end
       end
